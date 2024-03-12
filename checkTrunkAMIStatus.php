f59887f73d8c226489318cddaa274a521398153a
ossierra <162193058+ossierra@users.noreply.github.com>
2024-03-12 12:25:19 -0300

    1.1.3

--- checkTrunkAMIStatus.php
+++ checkTrunkAMIStatus.php
@@ -12,20 +12,28 @@ $ami_username = 'inceptia';
 $ami_password = 'jdK8mYqRMFwbuTXp5t';
 
 // Crear el socket de conexión AMI
-$socket = fsockopen($ami_host, $ami_port, $errno, $errstr, 10);
+$socket = fsockopen($ami_host,"5038", $ami_port, $errstr, 10);
+fputs($socket, "Action: Login\r\n");
+fputs($socket, "UserName: $ami_username\r\n");
+fputs($socket, "Secret: $ami_password\r\n\r\n");
 if (!$socket) {
     echo "Error al conectar a AMI: $errstr ($errno)\n";
     $logMessage = "Error al conectar a AMI: $errstr ($errno)\n";
     fwrite($fp, $logMessage);
     exit(1);
 }
+$wrets=fgets($socket,128);
 
-// Iniciar sesión AMI
-ami_login($socket, $ami_username, $ami_password);
+echo var_dump($wrets)."\n";
 
 // Ejecutar comando AMI para obtener información de los peers
 $response = ami_command($socket, 'Action: PJSIPShowContacts');
 
+fputs($socket, "Action: PJSIPShowContacts\r\n" );
+
+$wrets=fgets($socket,128);
+echo $wrets;
+exit;
 // Verificar si la respuesta contiene información de los peers
 if (strpos($response, 'Response: Success') !== false) {
     // Dividir la respuesta en líneas
@@ -46,37 +54,4 @@ if (strpos($response, 'Response: Success') !== false) {
 // Cerrar el socket de conexión AMI
 ami_logout($socket);
 fclose($fp);
-
-// Función para iniciar sesión en AMI
-function ami_login($socket, $username, $password) {
-    // Enviar comando de inicio de sesión AMI
-    $login_command = "Action: Login\r\nUsername: $username\r\nSecret: $password\r\n\r\n";
-    fputs($socket, $login_command);
-    // Leer y descartar la respuesta
-    $response = fgets($socket);
-    while ($response != "\r\n") {
-        $response = fgets($socket);
-    }
-}
-
-// Función para ejecutar un comando AMI
-function ami_command($socket, $command) {
-    // Enviar comando AMI
-    $command .= "\r\nAction: Logoff\r\n\r\n";
-    fputs($socket, $command);
-    // Leer y retornar la respuesta
-    $response = '';
-    while (!feof($socket)) {
-        $response .= fgets($socket);
-    }
-    return $response;
-}
-
-// Función para cerrar sesión en AMI
-function ami_logout($socket) {
-    // Enviar comando de cierre de sesión AMI
-    fputs($socket, "Action: Logoff\r\n\r\n");
-    // Cerrar el socket
-    fclose($socket);
-}
 ?>
