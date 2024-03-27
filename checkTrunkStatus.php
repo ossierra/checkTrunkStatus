0a5ca37056e391bb5fb81b268d85d5df852d45ec
ossierra <162193058+ossierra@users.noreply.github.com>
2024-03-27 13:46:34 -0300

    1.1.0

--- checkTrunkStatus.php
+++ checkTrunkStatus.php
@@ -1,118 +1,106 @@
 <?php
 $logFile = "/var/log/checkTrunkStatus.log";
-$fp = fopen($logFile, 'a');
-$logMessage = "Ejecutando el script\n";
-echo "Ejecutando el script\n";
-fwrite($fp, $logMessage);
-// Función para obtener la lista de peers PJSIP
-exec('asterisk -rx "pjsip show endpoints"',$output);
-
 $hostname = gethostname();
 $timestamp = time();
 $peers = [];
-//Este flag es para saber si miro o no miro el contact
-$flag = 0;
-$logMessage = "Entrando al loop\n";
+$fp = fopen($logFile, 'a');
+$logMessage = "$timestamp : Ejecutando el comando\n";
+fwrite($fp, $logMessage);
+echo "Ejecutando el comando\n";
+
+// Función para obtener la lista de peers PJSIP
+exec('asterisk -rx "pjsip list endpoints"',$output);
+
+$logMessage = "$timestamp : Entrando al loop\n";
 echo "Entrando al loop\n";
 fwrite($fp, $logMessage);
 foreach ($output as $line) {
-	if (strpos($line, 'Endpoint:') !== false) {
-		$parts = explode('  ', $line);
-		$info = array_values(array_filter($parts));
-		//var_dump($info)."\n";
-		$peer = $info[1];
-		$peer = trim($peer);
-		$status = $info[2];
-		$status = trim($status);
-		if (strpos($peer, "/") !== false) {
-			// Si contiene datos separados por "/", separar la cadena en partes
-			$numeros = explode("/", $peer);
+    if (strpos($line, 'Endpoint:') !== false) {
+        $parts = explode('  ', $line);
+        $info = array_values(array_filter($parts));
+        $peer = $info[1];
+        $peer = trim($peer);
+        if (strpos($peer, "/") !== false) {
+                $peer_part = explode('/', $peer);
+                $peer = $peer_part[0];
+        }
+        $status = $info[2];
+        $status = trim($status);
+        $canales_part  = explode(" ",$info[3]);
+        $canales = $canales_part[0];
+        if (strpos($peer, "<Endpoint") !== false) {
+                continue;
+        }else if(!is_numeric($peer)){
+            if($status == "Unavailable"){
+                $logMessage = "$timestamp : ALARMA!!! El peer: ".$peer." esta en estado: ".$status."\n";
+                fwrite($fp, $logMessage);
+                echo "ALARMA!!! El peer: ".$peer." esta en estado: ".$status."\n";
+                
+                $data = array(
+                    "version"=> "1.1",
+                    "facility" => "asterisk",
+                    "host"=> $hostname,
+                    "short_message"=> "El troncal $peer esta Unavaliable",
+                    "timestamp"=> $timestamp,
+                    "_trunk" => $peer,
+                    "_availability" => "false"
+                );
+                // Convertir los datos a formato JSON
+                $json_data = json_encode($data);
+            }else{
+                $logMessage = "$timestamp : El peer: ".$peer." esta en estado: ".$status." y tiene ".$canales." canales activos\n";
+                fwrite($fp, $logMessage);
+                echo "El peer: ".$peer." esta en estado: ".$status." y tiene ".$canales." canales activos\n";
+                
+                $data = array(
+                    "version"=> "1.1",
+                    "facility" => "asterisk",
+                    "host"=> $hostname,
+                    "short_message"=> "El troncal: ".$peer." esta en estado: ".$status." y tiene ".$canales." canales activos",
+                    "timestamp"=> $timestamp,
+                    "_trunk" => $peer,
+                    "_availability" => "true"
+                );
+                // Convertir los datos a formato JSON
+                $json_data = json_encode($data);
+            }
+        }
 
-			$peer = $numeros[0];
-		}
+        // Dirección y puerto del servidor Graylog
+        $host = 'graylog-inceptia.centralus.cloudapp.azure.com';
+        //$port = 12501;
+        $port = 12201;
 
-		if(!is_numeric($peer) and $peer != "<Endpoint/CID.....................................>"){
-			if($status == "Unavailable"){
-				echo "El peer ".$peer." se encuentra en estado: ".$status."\n";
-				//Si paso por aca prendo el flag en 1 para revisar el contact
-				$flag = 1;
-				$timestamp = date("Y-m-d H:i:s");
-				// Escribir un mensaje de inicio en el archivo de registro
-				$logMessage = "$timestamp - El peer ".$peer." se encuentra en estado: ".$status."\n";
-				fwrite($fp, $logMessage);
-				// Datos para enviar en el JSON
-				$data = array(
-					"version"=> "1.1",
-					"facility" => "asterisk",
-					"host"=> $hostname,
-					"short_message"=> "The trunk $peer is now Unavaliable",
-					"timestamp"=> $timestamp,
-        				"_trunk" => $peer,
-        				"_availability" => "false"
-				);
-    				// Convertir los datos a formato JSON
-    				$json_data = json_encode($data);
+        // Crear un socket UDP
+        $conexion = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        if ($conexion === false) {
+                echo "Error al crear el socket: " . socket_strerror(socket_last_error()) . PHP_EOL;
+                    $timestamp = date("Y-m-d H:i:s");
+                    // Escribir un mensaje de inicio en el archivo de registro
+                    $logMessage = "$timestamp - Error al crear el socket: " . socket_strerror(socket_last_error()) . PHP_EOL."\n";
+                    fwrite($fp, $logMessage);
+                return;
+        }
 
-				// Dirección y puerto del servidor Graylog
-    				$host = 'graylog-inceptia.centralus.cloudapp.azure.com';
-    				//$port = 12501;
-				$port = 12201;
+        // Enviar el JSON a través del socket UDP
+        $result = socket_sendto($conexion, $json_data, strlen($json_data), 0, $host, $port);
+        if ($result === false) {
+            echo "Error al enviar el mensaje: " . socket_strerror(socket_last_error()) . PHP_EOL;
+            $timestamp = date("Y-m-d H:i:s");
+            // Escribir un mensaje de inicio en el archivo de registro
+            $logMessage = "$timestamp - Error al enviar el mensaje: " . socket_strerror(socket_last_error()) . PHP_EOL."\n";
+            fwrite($fp, $logMessage);
+        } else {
+            echo "Alerta enviada a Graylog para el troncal $trunk via UDP." . PHP_EOL;
+            $timestamp = date("Y-m-d H:i:s");
+            // Escribir un mensaje de inicio en el archivo de registro
+            $logMessage = "$timestamp - Alerta enviada a Graylog para el troncal $trunk via UDP." . PHP_EOL."\n";
+            fwrite($fp, $logMessage);
+        }
 
-    				// Crear un socket UDP
-    				$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-    				if ($socket === false) {
-        				echo "Error al crear el socket: " . socket_strerror(socket_last_error()) . PHP_EOL;
-						$timestamp = date("Y-m-d H:i:s");
-						// Escribir un mensaje de inicio en el archivo de registro
-						$logMessage = "$timestamp - Error al crear el socket: " . socket_strerror(socket_last_error()) . PHP_EOL."\n";
-						fwrite($fp, $logMessage);
-        				return;
-    				}
-
-    				// Enviar el JSON a través del socket UDP
-    				$result = socket_sendto($socket, $json_data, strlen($json_data), 0, $host, $port);
-    				if ($result === false) {
-        				echo "Error al enviar el mensaje: " . socket_strerror(socket_last_error()) . PHP_EOL;
-						$timestamp = date("Y-m-d H:i:s");
-						// Escribir un mensaje de inicio en el archivo de registro
-						$logMessage = "$timestamp - Error al enviar el mensaje: " . socket_strerror(socket_last_error()) . PHP_EOL."\n";
-						fwrite($fp, $logMessage);
-    				} else {
-        				echo "Alerta enviada a Graylog para el troncal $peer via UDP." . PHP_EOL;
-						$timestamp = date("Y-m-d H:i:s");
-						// Escribir un mensaje de inicio en el archivo de registro
-						$logMessage = "$timestamp - Alerta enviada a Graylog para el troncal $peer via UDP." . PHP_EOL."\n";
-						fwrite($fp, $logMessage);
-    				}
-
-    				// Cerrar el socket
-    				socket_close($socket);
-
-			}else{
-				echo "El peer ".$peer." esta OK ".$status."\n";
-				$timestamp = date("Y-m-d H:i:s");
-				// Escribir un mensaje de inicio en el archivo de registro
-				$logMessage = "$timestamp - El peer ".$peer." esta OK ".$status."\n";
-				fwrite($fp, $logMessage);
-			}
-		}
-	}
-	if (strpos($line, 'Contact:') !== false and $flag == 1) {
-		$parts = explode(' ', $line);
-                $info = array_values(array_filter($parts));
-                //var_dump($info)."\n";
-                $contact = $info[1];
-                $status = $info[3];
-                if($peer != "<Endpoint/CID.....................................>"){
-                        if($status != "OK"){
-                                echo "El contact es: ".$contact." se encuentra en estado: ".$status."\n";
-                        }else{
-                                echo "El contact".$contact." esta OK\n";
-                        }
-                }
-	}
-	//Vuelvo a apagar el flag para el proximo peer
-	$flag = 0;
+        // Cerrar el socket
+        socket_close($conexion);
+    }
 }
 fclose($fp);
 ?>
